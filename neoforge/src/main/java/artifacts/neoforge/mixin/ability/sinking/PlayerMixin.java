bb2416a997b4d389535d256665f7f5bbfa8f6603
Wouter Pauwels <37985539+ochotonida@users.noreply.github.com>
2024-06-18 22:12:31 +0200

    Fix dig speed mixin

--- neoforge/src/main/java/artifacts/neoforge/mixin/ability/sinking/PlayerMixin.java
+++ neoforge/src/main/java/artifacts/neoforge/mixin/ability/sinking/PlayerMixin.java
@@ -1,4 +1,4 @@
-package artifacts.mixin.ability.sinking;
+package artifacts.neoforge.mixin.ability.sinking;
 
 import artifacts.registry.ModAbilities;
 import artifacts.util.AbilityHelper;
@@ -10,7 +10,7 @@ import org.spongepowered.asm.mixin.injection.At;
 @Mixin(Player.class)
 public class PlayerMixin {
 
-    @ModifyExpressionValue(method = "getDestroySpeed", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/entity/player/Player;isEyeInFluid(Lnet/minecraft/tags/TagKey;)Z"))
+    @ModifyExpressionValue(method = "getDigSpeed", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/entity/player/Player;isEyeInFluid(Lnet/minecraft/tags/TagKey;)Z"))
     private boolean isDestroySpeedAffectedByWater(boolean isInFluid) {
         Player player = (Player) (Object) this;
         return isInFluid && !AbilityHelper.hasAbilityActive(ModAbilities.SINKING.value(), player);
